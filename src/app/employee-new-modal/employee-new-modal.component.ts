5960259747f9fbc405b8005b2e6654a5d351861d
Eduardo Ribeiro <joseribeiroejrs@ufu.br>
2018-10-23 10:35:44 -0300

    Criando componente de employee new modal

---
+++ src/app/employee-new-modal/employee-new-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-new-modal',
+  templateUrl: './employee-new-modal.component.html',
+  styleUrls: ['./employee-new-modal.component.css']
+})
+export class EmployeeNewModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
